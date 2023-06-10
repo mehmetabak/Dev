307c484e9c91d425a41e7d40ee8995f2e05c5e7a
memoli0 <70433403+memoli0@users.noreply.github.com>
2023-06-10 16:57:06 +0300

    de

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ module.exports = {
       'res.cloudinary.com',
       'avatars.githubusercontent.com',
       'imgur.com',
+      'istockphoto.com'
+      'cdn1.iconfinder.com'
+      'notion.so'
     ],
   },
 };
