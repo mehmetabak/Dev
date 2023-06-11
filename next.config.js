b7f6866665219fb1aa36cb864e5983baac1d24c0
memoli0 <70433403+memoli0@users.noreply.github.com>
2023-06-11 10:51:01 +0300

    de

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
       'res.cloudinary.com',
       'avatars.githubusercontent.com',
       'imgur.com',
+      'i.imgur.com',
       'istockphoto.com',
       'cdn1.iconfinder.com',
       'media.licdn.com'
