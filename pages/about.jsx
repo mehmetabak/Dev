9e760be1c8708e48e829910d463ba0bd9701a320
memoli0 <70433403+memoli0@users.noreply.github.com>
2023-06-11 20:02:04 +0300

    de

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,9 +3,9 @@ const AboutPage = () => {
     <>
       <h2>As a Software Developer</h2>
       <h3>&nbsp;</h3>
-      <h3>👨‍💻 I know kotlin, java and python programming languages , </h3>
-      <h3>👀 I’m interested in learning new things , </h3>
-      <h3>🌱 I’m currently learning software development , </h3>
+      <h3>👨‍💻 I know kotlin, java and python programming languages, </h3>
+      <h3>👀 I’m interested in learning new things, </h3>
+      <h3>🌱 I’m currently learning software development. </h3>
     </>
   );
 };
