3fc7108b3a820df098a6338288ebaf363cfaefb9
memoli0 <70433403+memoli0@users.noreply.github.com>
2023-07-13 19:58:35 +0300

    test

--- pages/github.jsx
+++ pages/github.jsx
@@ -20,8 +20,8 @@ const GithubPage = ({ repos, user }) => {
             src={user.avatar_url}
             className={styles.avatar}
             alt={user.login}
-            width={50}
-            height={50}
+            width={60}
+            height={60}
           />
           <h3 className={styles.username}>{user.login}</h3>
         </div>
