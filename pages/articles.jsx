642a0144694191cd57a78a085d9c9fb96b709e63
Mehmet Abak <70433403+memoli0@users.noreply.github.com>
2024-03-30 17:14:07 +0300

    Update articles.jsx

--- pages/articles.jsx
+++ pages/articles.jsx
@@ -25,14 +25,7 @@ const ArticlesPage = ({ articles }) => {
 };
 
 export async function getStaticProps() {
-  const res = await fetch(
-    'https://dev.to/api/articles/me/published?per_page=6',
-    {
-      headers: {
-        'api-key': process.env.DEV_TO_API_KEY,
-      },
-    }
-  );
+  const res = await fetch(`https://dev.to/api/articles?username=memoli0`);
 
   const data = await res.json();
 
