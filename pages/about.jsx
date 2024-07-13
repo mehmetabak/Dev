c43b33c25fbcfb818c5f152d9c3a84ea14f8a3ac
Mehmet Abak <70433403+memoli0@users.noreply.github.com>
2024-07-13 13:35:47 +0300

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,7 @@
 const AboutPage = () => {
   return (
     <>
-      <h2 align="center">As a Software Developer:</h2>
+      <h2>As a Software Developer:</h2>
       <h3>&nbsp;</h3>
       <p align="justify">
         <h4>👨‍💻 Skilled in Java & Kotlin for Native Android Development</h4>
@@ -11,6 +11,7 @@ const AboutPage = () => {
       </p>
       <h3>&nbsp;</h3>
       <h3>Explore, Learn, Share 🚀</h3>
+      <h3>&nbsp;</h3>
       <p align="justify">
         <h4>  As a software developer, I'm always on the lookout for new technologies and eager to expand my knowledge. I document my journey, insights, and experiences on my blog <a href="https://m0s.vercel.app" target="_blank">m0s</a>. Dive into my portfolio at <a href="https://mehmetabak.is-a.dev/projects" target="_blank">mehmetabak.is-a.dev</a> to discover some of my proudest projects and achievements.</h4>
       </p>
