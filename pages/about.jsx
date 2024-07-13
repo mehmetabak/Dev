ccf4fe59b637e12bec0c2c241360c941325583f2
Mehmet Abak <70433403+memoli0@users.noreply.github.com>
2024-07-13 13:40:24 +0300

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -13,7 +13,7 @@ const AboutPage = () => {
       <h3>Explore, Learn, Share 🚀</h3>
       <h3>&nbsp;</h3>
       <p align="justify">
-        <h4>  As a software developer, I'm always on the lookout for new technologies and eager to expand my knowledge. I document my journey, insights, and experiences on my blog <a href="https://m0s.vercel.app" target="_blank">m0s</a>. Dive into my portfolio at <a href="https://mehmetabak.is-a.dev/projects" target="_blank">mehmetabak.is-a.dev</a> to discover some of my proudest projects and achievements.</h4>
+        <h4>  As a software developer, I'm always on the lookout for new technologies and eager to expand my knowledge. I document my journey, insights, and experiences on my blog <a href="https://m0s.vercel.app" target="_blank">m0s</a>. Dive into my portfolio at <Link href="/projects">mehmetabak.is-a.dev</Link> to discover some of my proudest projects and achievements.</h4>
       </p>
     </>
   );
