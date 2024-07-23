9ed01930b993c955f2d3b2c2c821a5c4f7b2729a
memoli0 <70433403+memoli0@users.noreply.github.com>
2024-07-23 10:51:47 +0300

    s

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const nextTranslate = require('next-translate');
 
 module.exports = nextTranslate({
   i18n: {
+    locales: ['en', 'tr'], 
+    defaultLocale: 'en', 
     localeDetection: false, 
   },
   images: {
