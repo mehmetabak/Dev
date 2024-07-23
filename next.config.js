aacd9a0fea78db9765f9a073abfdba5146b2ea58
memoli0 <70433403+memoli0@users.noreply.github.com>
2024-07-23 10:44:16 +0300

    s

--- next.config.js
+++ next.config.js
@@ -19,19 +19,3 @@ module.exports = nextTranslate({
     ],
   },
 });
-
-
-
-module.exports = {
-  images: {
-    domains: [
-      'res.cloudinary.com',
-      'avatars.githubusercontent.com',
-      'imgur.com',
-      'i.imgur.com',
-      'istockphoto.com',
-      'cdn1.iconfinder.com',
-      'media.licdn.com'
-    ],
-  },
-};
